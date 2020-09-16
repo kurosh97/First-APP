cc2087f523f146859a85135f74388f8ccc704b54
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2020-09-16 10:36:50 +0300

    add <Image>

--- views/Upload.js
+++ views/Upload.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Button, Container, Content, Form, Text} from 'native-base';
 import FormTextInput from '../components/FormTextInput';
 import {Image} from 'react-native';
 import useUploadForm from '../hooks/UploadHooks';
+import * as ImagePicker from 'expo-image-picker';
+// import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 
 const Upload = ({navigation}) => {
+  const [image, setImage] = useState(null);
+
+  const getPermissionAsync = async () => {
+    if (Platform.OS !== 'web') {
+      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
+
   const {
-    handleInputChange, uploadErrors} = useUploadForm();
+    handleInputChange,
+    uploadErrors,
+  } = useUploadForm();
 
 
   return (
